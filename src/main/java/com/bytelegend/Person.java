4b1ae755f299a3f452a2ce4b757bb3586b19f8da
jaryarbn <grandline501@gmail.com>
2022-09-14 10:32:48 +0800

    Answer by jaryarbn

--- src/main/java/com/bytelegend/Person.java
+++ src/main/java/com/bytelegend/Person.java
@@ -1,3 +1,15 @@
 package com.bytelegend;
 
-public class Person {}
+public class Person extends Life{
+
+    private final int age;
+
+    public Person(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    public void sayMyAge() {
+        System.out.println("my age is: " + age);
+    }
+}
