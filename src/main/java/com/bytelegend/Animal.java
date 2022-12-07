b1627d9bf925d77315ebf455ec245795a0e8047d
johnsonxiaoyu <1272304153@qq.com>
2022-12-07 03:06:07 +0000

    Update at 2022-12-07T03:06:07.776780Z

--- src/main/java/com/bytelegend/Animal.java
+++ src/main/java/com/bytelegend/Animal.java
@@ -1,3 +1,14 @@
 package com.bytelegend;
 
-public class Animal {}
+public class Animal extends Life{
+
+    public Animal(String name) {
+        super(name);
+    }
+
+    public void animalSound() {
+        System.out.println("woof");
+        System.out.println("meow");
+    }
+
+}
