30e05877cc2a98a97dae966397ce9c2f0050ae94
Andy Baxter <andrewbaxter439@gmail.com>
2023-02-25 22:35:24 +0000

    Try one submit

--- src/main/java/com/bytelegend/Animal.java
+++ src/main/java/com/bytelegend/Animal.java
@@ -1,3 +1,11 @@
 package com.bytelegend;
 
-public class Animal {}
+abstract class Animal extends Life {
+    public Animal(String name) {
+        super(name);
+    }
+
+    public void animalSound() {
+        System.out.println("woof");
+    }
+}
