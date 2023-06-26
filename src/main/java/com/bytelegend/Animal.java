167e8a832aa8a4bc959d57c317bb26a651afa8c5
liukun <Aa123456>
2023-06-26 09:02:03 +0800

    change

--- src/main/java/com/bytelegend/Animal.java
+++ src/main/java/com/bytelegend/Animal.java
@@ -1,3 +1,16 @@
 package com.bytelegend;
 
-public class Animal {}
+public class Animal extends Life {
+    private final String sound;
+
+    public Animal(String name, String sound) {
+        super(name);
+        this.sound = sound;
+    }
+
+    public void animalSound() {
+        System.out.println(sound);
+    }
+
+    ;
+}
